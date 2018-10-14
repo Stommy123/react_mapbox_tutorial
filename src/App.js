be0cfffca97d2bc5a563f4626ee70d3082cd2af4
Stommy123 <Stommy49@Yahoo.com>
2018-10-14 15:08:20 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Map from './components/Map.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <Map></Map>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import mapboxgl from 'mapbox-gl'
+import './marker.css'
+import ReactDOMServer from 'react-dom/server'
+import Popup from './Popup'
+import axios from 'axios'
+
+class Map extends Component {
+  async componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYW5keXdlaXNzMTk4MiIsImEiOiJIeHpkYVBrIn0.3N03oecxx5TaQz7YLg2HqA'
+    const mapOptions = {
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      zoom: 12,
+      center: [-80.2044, 25.8028]
+    };
+    const geolocationOptions = {
+      enableHighAccuracy: true,
+      maximumAge: 30000,
+      timeout: 27000
+    };
+    await this.createMap(mapOptions, geolocationOptions)
+  }
+
+  createMap = (mapOptions, geolocationOptions) => {
+    this.map = new mapboxgl.Map(mapOptions);
+    const map = this.map;
+
+    // map.addControl(
+    //   new MapboxGeocoder({
+    //     accessToken: mapboxgl.accessToken
+    //   })
+    // );
+
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+          enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    }));
+
+    var nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-right');
+
+    map.on('load', (event) => {
+      this.fetchPlaces();
+    })
+  }
+
+  fetchPlaces = () => {
+    const locations = [
+      {
+        name: 'Wyncode',
+        longitude: -80.2044,
+        latitude: 25.8028
+      },
+      {
+        name: "Joe's Stone Crab",
+        longitude: -80.1353,
+        latitude: 25.7689
+      },
+      {
+        name: "Zuma",
+        longitude: -80.1896,
+        latitude: 25.7705
+      },
+    ]
+    const map = this.map;
+    const self= this;
+    locations.forEach((location, i) => {
+      let elm = document.createElement('div')
+      elm.className = "mapbox-marker"
+      let popup = new mapboxgl.Popup({ offset: 25})
+      .setHTML(ReactDOMServer.renderToStaticMarkup(
+        <Popup location={location}></Popup>
+      ))
+      let marker = new mapboxgl.Marker(elm)
+      .setLngLat([location.longitude, location.latitude])
+      .setPopup(popup)
+      marker.addTo(map)
+    })
+  }
+
+  render() {
+    const style = {
+      width: '100%',
+      height: '500px',
+      backgroundColor: 'azure'
+    };
+    return (
+      <div style={style} ref={el => this.mapContainer = el} />
+    )
+  }
+}
+
+
+export default Map
