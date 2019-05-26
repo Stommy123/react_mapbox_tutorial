81d9b8adc50e68dbc2a8fc51848fc28ec5ad6863
tommy <stommy49@yahoo.com>
2019-05-25 22:25:28 -0400

    fixed load position

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,17 @@
+export const geolocationOptions = {
+  enableHighAccuracy: true,
+  maximumAge: 10000,
+  timeout: 10000
+};
+
 const getCurrentPosition = _ =>
-  new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
+  new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject, geolocationOptions));
 
 export const loadPosition = async _ => {
   try {
     return await getCurrentPosition();
   } catch (error) {
+    console.log("catching", error);
     return {
       coords: {
         longitude: -80.2044,
@@ -35,11 +42,5 @@ export const popupRenderer = (props = {}) => `
     <p>${props.name}</p>
   </div>
 `;
-
-export const geolocationOptions = {
-  enableHighAccuracy: true,
-  maximumAge: 30000,
-  timeout: 27000
-};
 
 export const MAPBOX_API_KEY = "pk.eyJ1IjoiYW5keXdlaXNzMTk4MiIsImEiOiJIeHpkYVBrIn0.3N03oecxx5TaQz7YLg2HqA";
